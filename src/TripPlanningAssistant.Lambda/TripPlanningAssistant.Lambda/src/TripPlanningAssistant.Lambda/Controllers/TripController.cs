2f65ff9598f5e2effa5920fe978d99eb969cf24a
Johnny Nguyen <johnny.nguyen@helixleisure.com>
2024-07-12 15:06:48 +0800

    Initial creation for lambda project

---
+++ src/TripPlanningAssistant.Lambda/TripPlanningAssistant.Lambda/src/TripPlanningAssistant.Lambda/Controllers/TripController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Supabase;
+using System.Text.Json;
+using TripPlanningAssistant.Lambda.Models;
+using TripPlanningAssistant.API.Services;
+
+namespace TripPlanningAssistant.Lambda.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TripController : ControllerBase
+    {
+        private readonly ILogger<TripController> _logger;
+        private readonly Client _client;
+        private readonly AWSBedrockService _awsBedrockService;
+
+        public TripController(ILogger<TripController> logger, 
+            Client client,
+            AWSBedrockService awsBedrockService)
+        {
+            _logger = logger;
+            _client = client;
+            _awsBedrockService = awsBedrockService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<string>> SematicSearch(string input, Single matchThreshold = 0.5f, int count = 10)
+        {
+            var inputEmbedding = await _awsBedrockService.GenerateEmbeddingsResponseAsync(input);
+
+            var result = await _client.Rpc("match_documents", new 
+            {
+                query_embedding = inputEmbedding, // pass the query embedding
+                match_threshold = matchThreshold, // choose an appropriate threshold for your data
+                match_count = count, // choose the number of matches
+            });
+
+            var convertedResult = JsonSerializer.Deserialize<IEnumerable<BaseModel>>(result.Content ?? "");
+            return convertedResult?.Select(x => x.sentences) ?? new List<string>();
+        }
+    }
+}
---
+++ src/TripPlanningAssistant.Lambda/TripPlanningAssistant.Lambda/src/TripPlanningAssistant.Lambda/Models/BaseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TripPlanningAssistant.Lambda.Models
+{
+    public class BaseModel
+    {
+        public int id { get; set; }
+
+        public string sentences { get; set; } = string.Empty;
+    }
+}
