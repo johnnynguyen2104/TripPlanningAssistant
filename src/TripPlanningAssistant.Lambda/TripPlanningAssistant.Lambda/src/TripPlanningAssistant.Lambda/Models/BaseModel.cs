8531c527bad921c19957d8e182699002a8ccb87b
Johnny Nguyen <johnny.nguyen@helixleisure.com>
2024-07-13 15:21:11 +0800

    Removed unused project

--- src/TripPlanningAssistant.Lambda/TripPlanningAssistant.Lambda/src/TripPlanningAssistant.Lambda/Models/BaseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TripPlanningAssistant.Lambda.Models
-{
-    public class BaseModel
-    {
-        public int id { get; set; }
-
-        public string sentences { get; set; } = string.Empty;
-    }
-}
